419a787003fd188cbdcc28744a2944cb1eb5404c
mohamedisse <mohamedr201@gmail.com>
2017-02-23 02:16:33 -0500

    Angular is hooked to the backend

---
+++ public/javascript/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by mohamedisse on 2/23/17.
+ */
+(function(){
+    'use strict';
+
+    var app = angular.module('ViewrApp',[]);
+    app.controller('myController', function($scope) {
+        $scope.message = "Hello"
+    });
+    
+})();
